d70cb6d31d51edd312b9daf8f646e931fc1ed11a
kiry665 <51780313+kiry665@users.noreply.github.com>
2022-10-23 10:04:39 +0500

    4

---
+++ Работа с SQLite/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(189, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(608, 545);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 610);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Работа с SQLite/Form1.cs
@@ -0,0 +1,46 @@
+using System.Data.SQLite;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection(@"Data Source = db.sqlite"))
+            {
+                connection.Open();
+                SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Levels WHERE Number_Level = 3", connection);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())   // построчно считываем данные
+                    {
+                        var array = reader.GetValue(1);
+                        int row = reader.GetInt32(2);
+                        int col = reader.GetInt32(3);
+                        int[,] patern = new int[row,col];
+                        string[] rows = array.ToString().Split(';');
+
+                        dataGridView1.RowCount = row;
+                        dataGridView1.ColumnCount = col;
+
+                        for (int i = 0; i < row; i++)
+                        {
+                            string[] cells = rows[i].Split(',');
+                            for (int j = 0; j < col; j++)
+                            {
+                                patern[i, j] = Convert.ToInt32(cells[j]);
+                                dataGridView1.Rows[i].Cells[j].Value = patern[i, j];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
